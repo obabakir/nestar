e6b662685f6a4e7c514c4fe7e6356650eec88fe4
obabakir <bubufighting01@gmail.com>
2026-08-22 14:42:19 +0500

    feat: develop getMember api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -9,6 +9,7 @@ import { Message } from '../../libs/enums/common.enum';
 import * as bcrypt from 'bcrypt';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class MemberService {
@@ -78,8 +79,16 @@ export class MemberService {
 		return result;
 	}
 
-	public async getMember(): Promise<string> {
-		return 'getMember exacuted !';
+	public async getMember(targetId: ObjectId): Promise<Member> {
+		const search: T = {
+			_id: targetId,
+			memberStatus: {
+				$in: [MemberStatus.ACTIVE, MemberStatus.BLOCK],
+			},
+		};
+		const targetMember = await this.memberModel.findOne(search).exec();
+		if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		return targetMember;
 	}
 
 	public async getAllMembersByAdmin(): Promise<string> {
