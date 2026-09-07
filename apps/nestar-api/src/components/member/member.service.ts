34767d0cc789875729c96eb050e523b4b4ab86b9
obabakir <bubufighting01@gmail.com>
2026-09-07 13:34:48 +0900

    fix: modify getMember business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -16,11 +16,13 @@ import { ViewInput } from '../../libs/dto/view/viewInput';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
 import { LikeService } from '../like/like.service';
+import { Follower, Following, MeFollowed } from '../../libs/dto/follow/follow';
 
 @Injectable()
 export class MemberService {
 	constructor(
 		@InjectModel('Member') private readonly memberModel: Model<Member>,
+		@InjectModel('Follow') private readonly followModel: Model<Follower | Following>,
 		private authService: AuthService,
 		private viewService: ViewService,
 		private likeService: LikeService,
@@ -134,10 +136,20 @@ export class MemberService {
 		targetMember.meLiked = await this.likeService.checkLikeExistence(likeInput);
 
 		// TODO: meFollowed
+		targetMember.meFollowed = await this.checkSubscription(memberId, targetId);
 
 		return targetMember;
 	}
 
+	// =====
+
+	private async checkSubscription(followerId: ObjectId, followingId: ObjectId): Promise<MeFollowed[]> {
+		const result = await this.followModel.findOne({ followingId: followingId, followerId: followerId }).exec();
+
+		return result ? [{ followerId: followerId, followingId: followingId, myFollowing: true }] : [];
+	}
+
+	// ====
 	public async getAgents(memberId: ObjectId, input: AgentsInquiry): Promise<Members> {
 		const { text } = input.search;
 		const match: T = { memberType: MemberType.AGENT, memberStatus: MemberStatus.ACTIVE };
--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -2,6 +2,7 @@ import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { ObjectId } from 'mongoose';
 import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
 import { MeLiked } from '../like/like';
+import { MeFollowed } from '../follow/follow';
 
 @ObjectType()
 export class Member {
@@ -82,9 +83,12 @@ export class Member {
 	@Field(() => String, { nullable: true })
 	accessToken?: string;
 
-	// from agrigation
+	// from agrigation (DTO => MeLiked & MeFolowed)
 	@Field(() => [MeLiked], { nullable: true })
 	meLiked?: MeLiked[];
+
+	@Field(() => [MeFollowed], { nullable: true })
+	meFollowed?: MeFollowed[];
 }
 @ObjectType()
 export class TotalCounter {
